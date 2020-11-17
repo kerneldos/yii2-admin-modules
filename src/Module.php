33e08af836ba211eacbfb138b05e94daf7d2ed0c
Мишин Артём <mishin.a.e.1988@gmail.com>
2020-11-17 11:11:16 +0300

    Move bootstrap method to main module class

--- src/Module.php
+++ src/Module.php
@@ -3,8 +3,19 @@
 
 namespace kerneldos\extmodule;
 
+use yii\base\Application;
+use yii\base\BootstrapInterface;
 
-class Module extends \yii\base\Module {
+class Module extends \yii\base\Module implements BootstrapInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $id = 'admin';
+
+    /**
+     * {@inheritdoc}
+     */
     public $layout = 'admin';
 
     /**
@@ -15,7 +26,8 @@ class Module extends \yii\base\Module {
     /**
      * {@inheritdoc}
      */
-    public function init() {
+    public function init()
+    {
         parent::init();
 
         if (($gii = \Yii::$app->getModule('gii')) !== null) {
@@ -28,16 +40,95 @@ class Module extends \yii\base\Module {
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritDoc
+     * @throws \yii\base\InvalidConfigException
      */
-    public function beforeAction($action) {
-        foreach ($this->getModules() as $name => $module) {
-            \Yii::$app->view->params['menuItems'][] = [
+    public function bootstrap($app)
+    {
+        if ($app instanceof \yii\console\Application) {
+            return;
+        }
+
+        $hasInstance = false;
+        foreach ($app->getModules() as $id => $module) {
+            $obj = \Yii::createObject($module);
+
+            if ($obj instanceof $this) {
+                $hasInstance = true;
+                $this->id = $id;
+                break;
+            }
+        }
+
+        if (!$hasInstance) {
+            $app->setModule($this->id, ['class' => 'kerneldos\extmodule\Module']);
+        }
+
+        $modules = \kerneldos\extmodule\models\Module::find()
+            ->where(['is_active' => true])
+            ->indexBy('name')
+            ->asArray()
+            ->all();
+
+        $baseNamespace = 'app\\modules';
+        $frontendNamespace = 'controllers\\frontend';
+        $backendNamespace = 'controllers\\backend';
+
+        foreach ($modules as $name => $module) {
+            $app->setModule($name, [
+                'class' => $module['class'],
+                'controllerNamespace' => join('\\', [$baseNamespace, $name, $frontendNamespace]),
+                'viewPath' => "@app/modules/$name/views/frontend",
+            ]);
+            $app->getModule($this->id)->setModule($name, [
+                'class' => $module['class'],
+                'controllerNamespace' => join('\\', [$baseNamespace, $name, $backendNamespace]),
+                'viewPath' => "@app/modules/$name/views/backend",
+            ]);
+
+            if ($module['is_bootstrap']) {
+                $app->bootstrap[] = $name;
+            }
+
+            if ($module['is_main']) {
+                $app->getUrlManager()->addRules(['' => $name], false);
+            }
+
+            $app->view->params['menuItems'][] = [
                 'label' => ucwords($name),
                 'url' => "/$this->id/$name",
             ];
         }
 
-        return parent::beforeAction($action);
+        $this->setUrlRules($app);
+    }
+
+    /**
+     * @param Application $app
+     */
+    protected function setUrlRules($app)
+    {
+        $app->getUrlManager()->addRules([
+            [
+                'class' => 'yii\web\GroupUrlRule',
+                'prefix' => $this->id,
+                'rules' => [
+                    '' => 'default/index',
+
+                    '<_a:create>'                   => 'default/<_a>',
+                    '<_a:view|update|delete>/<id>'  => 'default/<_a>',
+
+                    '<module>/<_a:view|update|delete>/<id>' => '<module>/default/<_a>',
+
+                    '<module>'                          => '<module>/default/index',
+                    '<module>/<controller>'             => '<module>/<controller>/index',
+                    '<module>/<controller>/<action>'    => '<module>/<controller>/<action>',
+                ],
+            ],
+
+            '<module>'                          => '<module>/default/index',
+            '<module>/<controller>'             => '<module>/<controller>/index',
+            '<module>/<controller>/<action>'    => '<module>/<controller>/<action>',
+        ]);
     }
 }
